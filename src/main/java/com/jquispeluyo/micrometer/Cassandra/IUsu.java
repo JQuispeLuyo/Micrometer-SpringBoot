d80ad005d9b14e0ec7a656e6633e088487e69e78
MartinSamanArata2018 <martin.saman@vallegrande.edu.pe>
2020-03-03 20:40:45 -0500

    avance

---
+++ src/main/java/com/jquispeluyo/micrometer/Cassandra/IUsu.java
@@ -0,0 +1,12 @@
+package com.jquispeluyo.micrometer.Cassandra;
+
+
+
+import org.springframework.data.cassandra.repository.CassandraRepository;
+
+
+public interface IUsu extends CassandraRepository<Usu, String> {
+
+
+
+}
