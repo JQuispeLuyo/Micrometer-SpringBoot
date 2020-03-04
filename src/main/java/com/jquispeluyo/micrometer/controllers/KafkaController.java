e96fc4c6b8f6cdf6f459694f0f0f6da7eb079f4c
José Luis Quispe Luyo <jose.quispe@vallegrande.edu.pe>
2020-03-04 15:53:09 -0500

    kafka

---
+++ src/main/java/com/jquispeluyo/micrometer/controllers/KafkaController.java
@@ -0,0 +1,22 @@
+package com.jquispeluyo.micrometer.controllers;
+
+import com.jquispeluyo.micrometer.kafka.KafkaProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/kafka")
+public class KafkaController {
+
+    @Autowired
+    KafkaProducer kafkaProducer;
+
+    @GetMapping("message/{message}")
+    public void producerMessage(@PathVariable("message") String message){
+        kafkaProducer.send(message);
+    }
+
+}
