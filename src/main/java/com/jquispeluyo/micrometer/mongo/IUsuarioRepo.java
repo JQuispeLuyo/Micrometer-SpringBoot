31dcc449bd16ca04822f47b3725e004e72cae3c4
JCasasChacaliaza <james.casas@vallegrande.edu.pe>
2020-03-05 10:38:57 -0500

    cassandra

--- src/main/java/com/jquispeluyo/micrometer/mongo/IUsuarioRepo.java
+++
@@ -1,9 +0,0 @@
-package com.jquispeluyo.micrometer.mongo;
-
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface IUsuarioRepo extends MongoRepository<Usuario, String> {
-	
-	Usuario findByNombre(String nombre);
-}
